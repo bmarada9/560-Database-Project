da7a9734a27f170228097c0933610a8fe68deecd
Cherrera543 <cherrera543@ksu.edu>
2021-11-28 20:51:00 -0600

    Added Windows Forms

---
+++ DataAccessDemo2/formsProj/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FindAJob
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void createProfile_Click(object sender, EventArgs e)
+        {
+            createProfile cp = new createProfile();
+            cp.Show();
+        }
+
+        private void addJob_Click(object sender, EventArgs e)
+        {
+            addJob aj = new addJob();
+            aj.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            displayPage display = new displayPage();
+            display.Show();
+        }
+    }
+}
--- DataAccessDemo2/formsProj/Program.cs
+++ DataAccessDemo2/formsProj/Program.cs
@@ -16,7 +16,7 @@ namespace FindAJob
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new welcomePage());
+            Application.Run(new Form1());
         }
     }
 }
